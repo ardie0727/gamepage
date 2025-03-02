bc71b7bdf39aa31e710892752361d36957136f39
ardie0727 <drohit7789@gmail.com>
2025-03-02 21:15:08 +0530

    updated git ignore

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hgqcenrwbxkygodqkgxa.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhncWNlbnJ3YnhreWdvZHFrZ3hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5MjIzNDcsImV4cCI6MjA1NjQ5ODM0N30.isQM9R1wmRMdjxIw4KkLSqrHkrKXEIjgPaBL_1VVU_s';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
